c0e0f5aa8704af32cf5f6c74e46e4a4c842ffadb
Alicia <alicia@ringover.com>
2019-11-24 20:29:06 +0100

    html template finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+class HelloWorld {
+  constructor() {
+    this.say = 'Hello World';
+  }
+
+  speak(say = this.say) {
+    console.log(say);
+  }
+}
+
+const helloWorld = new HelloWorld();
+
+helloWorld.speak();
+helloWorld.speak('bonjour tout le monde');
